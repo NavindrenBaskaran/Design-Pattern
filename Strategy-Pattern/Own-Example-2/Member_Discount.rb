aa3b4b86161a25becc1a2fde9af558165b7c1bc0
Navindren <navin25@hotmail.com>
2017-04-05 00:37:02 +0800

    Add line to print discount type

--- Strategy-Pattern/Own-Example-2/Member_Discount.rb
+++ Strategy-Pattern/Own-Example-2/Member_Discount.rb
@@ -2,6 +2,7 @@ require './Discount.rb'
 
 class Member_Discount < Discount
   def calc(customer)
+    puts "Member Price"
     customer.chargeable - (customer.chargeable * 0.5) + (customer.chargeable * 0.06)
   end
 end
--- Strategy-Pattern/Own-Example-2/Student_Discount.rb
+++ Strategy-Pattern/Own-Example-2/Student_Discount.rb
@@ -2,6 +2,7 @@ require './Discount.rb'
 
 class Student_Discount < Discount
   def calc(customer)
+    puts "Student Price"
     customer.chargeable - (customer.chargeable * 0.1) + (customer.chargeable * 0.06)
   end
 end
